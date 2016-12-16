99434c8a108e28bb3fcd1c64d1e5a88a230ef3b5
nathaneastwood <nathan.eastwood@icloud.com>
2016-12-16 10:21:00 +0000

    Update hexMapJson data so it contains characters and not factors

--- data-raw/createHexMapJson.R
+++ data-raw/createHexMapJson.R
@@ -11,5 +11,8 @@ createHexMapJson <- function() {
                       package = "employmentProspects")
   data <- rgdal::readOGR(file)
   colnames(data@data) <- c("id", "lad15cd", "lad15nm")
+  data@data$id <- as.character(data@data$id)
+  data@data$lad15cd <- as.character(data@data$lad15cd)
+  data@data$lad15nm <- as.character(data@data$lad15nm)
   data
 }
