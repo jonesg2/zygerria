55a4f2f38be6b5ab27ce40f79e1f1b32ea673631
nathaneastwood <nathan.eastwood@icloud.com>
2017-01-05 12:29:59 +0000

    Replace old scatter plot function with new plotly version

--- R/compositeScatter.R
+++ R/compositeScatter.R
@@ -3,61 +3,73 @@
 #' Create a scatter plot of the composite variables
 #'
 #' @param data The employment data, see details for more information.
-#' @param plotly Logical; defaults to TRUE. Whether to return the plot as an
-#'   interactive plotly graphic or not.
 #'
 #' @details
 #' The \code{data} needs to contain \code{measureA}, \code{measureB} and the
-#' employment rate statistics, where employment rate is defined as a categorical
-#' variable; red is <73%, amber is 73-77% and green is >77%.
+#' employment rate statistics.
 #'
 #' @author Nathan Eastwood
 #'
-#' @importFrom ggplot2 ggplot geom_vline geom_hline geom_point aes
-#'   scale_colour_manual scale_shape_manual xlim ylim labs coord_fixed theme_bw
-#' @importFrom plotly ggplotly
+#' @importFrom plotly plot_ly add_trace layout
 #'
 #' @export
-compositeScatter <- function (data, plotly = TRUE) {
-  p <-
-    ggplot2::ggplot(data) +
-    ggplot2::geom_vline(xintercept = 50) +
-    ggplot2::geom_hline(yintercept = 50) +
-    ggplot2::geom_point(
-      ggplot2::aes(x = measure_a,
-                   y = measure_b,
-                   colour = emp_rate_hml,
-                   shape = emp_rate_hml),
-      size = 2
-    ) +
-    ggplot2::scale_colour_manual(
-      "Current\nEmployment\nRate",
-      values = c("red" = "red",
-                 "orange" = "#FFC200",
-                 "green" = "green3"),
-      labels = c("red" = "<73%",
-                 "orange" = "73-77%",
-                 "green" = ">77%")
-    ) +
-    ggplot2::scale_shape_manual(
-      "Current\nEmployment\nRate",
-      values = c("red" = 15,
-                 "orange" = 18,
-                 "green" = 17),
-      labels = c("red" = "<73%",
-                 "orange" = "73-77%",
-                 "green" = ">77%")
-    ) +
-    ggplot2::xlim(c(0, 100)) +
-    ggplot2::ylim(c(0, 100)) +
-    ggplot2::labs(x = "Fragility of Current Jobs",
-                  y = "Conditions for Future Jobs Growth") +
-    ggplot2::coord_fixed() +
-    ggplot2::theme_bw()
+compositeScatter <- function(data) {
 
-  if (plotly) {
-    plotly::ggplotly(p, tooltip = c("x", "y"), source = "compscat")
-  } else {
-    p
-  }
+  data <- data %>%
+    mutate(
+      emp_rate_hml = if_else(
+        emp_rate < 73, "red",
+        if_else(emp_rate >= 73 & emp_rate < 78,
+                "orange",
+                "green"))
+    )
+
+  pal <- c("green3", "#FFC200", "red")
+  pal <- setNames(pal, c("green", "orange", "red"))
+
+  p <- plotly::plot_ly(
+    symbols = c("circle", "x", "o"),
+    colors = pal
+  ) %>%
+    plotly::add_trace(
+      x = c(0, 100),
+      y = c(50, 50),
+      type = "scatter",
+      mode = "lines",
+      color = I("grey51"),
+      showlegend = FALSE
+    ) %>%
+    plotly::add_trace(
+      x = c(50, 50),
+      y = c(0, 100),
+      type = "scatter",
+      mode = "lines",
+      color = I("grey51"),
+      showlegend = FALSE
+    ) %>%
+    plotly::add_trace(
+      data = data,
+      type = "scatter",
+      mode = "markers",
+      x = ~measure_a,
+      y = ~measure_b,
+      color = ~emp_rate_hml,
+      symbol = ~emp_rate_hml,
+      text = ~paste(
+        "Region: ", la_name,
+        "<br>Measure A: ", measure_a,
+        "<br>Measure B: ", measure_b
+      ),
+      hoverinfo = "text",
+      marker = list(
+        size = 10
+      )
+    ) %>%
+    plotly::layout(
+      xaxis = list(title = "Fragility of Current Jobs"),
+      yaxis = list(title = "Conditions for Future Jobs Growth"),
+      legend = list(x = 100, y = 0.5)
+    )
+
+  p
 }
