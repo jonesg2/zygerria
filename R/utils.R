7b893878c0cb63f2aace08d054bca46d13ac4357
nathaneastwood <nathan.eastwood@icloud.com>
2017-01-05 14:26:54 +0000

    Complete missing documentation

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,16 @@
 #' Round up to nearest number
 #'
+#' @param x A vector.
+#' @param to The nearest digit to round up to.
+#'
 #' @author Nathan Eastwood
 roundUp <- function(x, to = 10) {
   to * (x %/% to + as.logical(x %% to))
 }
 
 #' Round down to nearest 10
+#'
+#' @param x A vector
 #'
 #' @author Nathan Eastwood
 roundDown <- function(x) {
